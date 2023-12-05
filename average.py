737f5135e3e25cd3798e77e6ed025eeb48fb2285
RAfael <theblackshd@hotmail.com>
2023-12-05 19:30:00 -0300

    aquilo lá e isso aqui

--- average.py
+++ average.py
@@ -3,12 +3,17 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn import tree
 import matplotlib.pyplot as plt
 from io import StringIO  
 from graphviz import Source
 from random import randint
+from sklearn.tree import export_graphviz
+import os
+
+# Set the seed value
+np.random.seed(42)
 
 qnt_max_depth = 0
 
@@ -21,41 +26,69 @@ def train():
     X_train, X_test, y_train, y_test = train_test_split(data.drop('heart_attack_risk', axis=1), data['heart_attack_risk'], test_size=0.2)
 
     # Create a decision tree classifier
-    clf = RandomForestClassifier(max_depth=4)
+    clf = RandomForestClassifier(max_depth=qnt_max_depth)
+
     # Fit the model to the data
     clf.fit(X_train, y_train)
+    clf.threshold = 0.2
 
     return X_train, X_test, y_train, y_test, clf
 
 
-def measure_accuracy(clf, X_test, y_test):
+def measure_metrics(clf, X_test, y_test):
+    """
+        Calcula as métricas de precisão, acurácia e recall para um classificador.
+
+        Acurácia é o nível geral de acertos do modelo
+        Precisão é importante para minimizar o número de falsos positivos
+        Recall é importante para minimizar o número de falsos negativos
+
+        Parâmetros:
+            clf (Classificador): Classificador treinado.
+            X_test (array-like): Conjunto de dados de teste.
+            y_test (array-like): Rótulos dos dados de teste.
+
+        Retornos:
+            accuracy (float): Acurácia do classificador.
+            precision (float): Precisão do classificador.
+            recall (float): Recall do classificador.
+    """
     # Faz previsões
     y_pred = clf.predict(X_test)
 
-    # Calcula a acurácia
+    # Representa a taxa de acertos do modelo
     accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average='macro')
 
-    return accuracy, precision
+    # De todas as previsões feitas e positivas, quantas eram verdade?
+    precision = precision_score(y_test, y_pred, average='macro', zero_division=0)
+
+    # De todos os dados que deveriam ser previstos como verdade, quantos foram previstos corretamente?
+    recall = recall_score(y_test, y_pred, average='macro', zero_division=0)
+
+    return accuracy, precision, recall
 
 
 def main():
     X_train, X_test, y_train, y_test, clf = train()
-    precisao, acuracia = measure_accuracy(clf, X_test, y_test)
+    precisao, acuracia, recall = measure_metrics(clf, X_test, y_test)
 
-    return precisao, acuracia
+    return precisao, acuracia, recall
 
 
 if __name__ == "__main__":
     for j in range(0, 1):
-        qnt_max_depth = 10
-        n = 100
+        qnt_max_depth = 5
+        n = 10
+
         acuracia_total = 0
         precisao_total = 0
+        recall_total = 0
+
         for i in range(0, n):
-            acuracia_atual, precisao_atual = main()
+            acuracia_atual, precisao_atual, recall_atual = main()
             acuracia_total += acuracia_atual
             precisao_total += precisao_atual
+            recall_total   += recall_atual
     
 
-        print("Precisao: {} --- Acuracia: {}".format(precisao_total/n, acuracia_total/n))
+        print("Acuracia: {} --- Precisão: {} --- Recall: {}".format(acuracia_total/n, precisao_total/n, recall_total/n))
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score
 from sklearn import tree
 import matplotlib.pyplot as plt
-from io import StringIO  
+import seaborn as sn
 from graphviz import Source
 
+# Set the seed value
+np.random.seed(42)
 
 def train():
     # Load the heart attack data
@@ -27,10 +29,24 @@ def measure_accuracy(clf, X_test, y_test):
     # Faz previsões
     y_pred = clf.predict(X_test)
 
-    # Calcula a acurácia
+    cm = confusion_matrix(y_test, y_pred, labels=clf.classes_)
+    df_cfm = pd.DataFrame(cm, index = ["0", "1", "2"], columns = ["0", "1", "2"])
+    plt.figure(figsize = (10,7))
+
+    cfm_plot: plt.Axes = sn.heatmap(df_cfm, annot=True)
+    cfm_plot.set(xlabel='PREVISÃO', ylabel='VERDADE')
+    cfm_plot.figure.savefig("cfm.png")
+
+    # Calcula a acurácia (TP + TN) / (TP + TN + FP + FN)
     accuracy = accuracy_score(y_test, y_pred)
 
-    print('Acurácia:', accuracy)
+    print('Acurácia:', accuracy, end="")
+
+    precision = precision_score(y_test, y_pred, average='macro', zero_division=0)
+    print(', Precisão:', precision, end="")
+
+    recall = recall_score(y_test, y_pred, average='macro', zero_division=0)
+    print(', Recall:', recall)
 
 
 def predict(clf, patient):
@@ -50,7 +66,7 @@ def main():
     X_train, X_test, y_train, y_test, clf = train()
     measure_accuracy(clf, X_test, y_test)
 
-    data = [[21,0,120,80,100,0,0]]
+    data = [[40,0,125,80,220,1,1]]
     df = pd.DataFrame(data, columns=['age','sex','systolic_blood_pressure','diastolic_blood_pressure','cholesterol','smoking','diabetes'])
     predict(clf, df)
 
@@ -62,6 +78,8 @@ def main():
     png_bytes = graph.pipe(format='png')
     with open('dtree_pipe.png','wb') as f:
         f.write(png_bytes)
+
+    # Generate confusion matrix
 
 
 if __name__ == "__main__":
